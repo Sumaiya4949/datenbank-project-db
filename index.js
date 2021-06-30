c2b5d58ff225ae014b1152a2ccb9558947d93d69
sinthy <sumaiya15-4949@diu.edu.bd>
2021-06-30 15:41:52 +0200

    Insert subjects and classes

--- index.js
+++ index.js
@@ -2,6 +2,78 @@ const pgp = require("pg-promise")()
 const fs = require("fs")
 const chalk = require("chalk")
 const { v4: uuid } = require("uuid")
+const { SHA3 } = require("sha3")
+
+function getPasswordHash(password) {
+  const hash = new SHA3(512)
+  hash.update(password)
+  return hash.digest("hex")
+}
+
+const MASTER_PASSWORD = "123456"
+const MASTER_PASSWORD_HASH = getPasswordHash(MASTER_PASSWORD)
+
+async function populateDB(db) {
+  // Insert 1 admin
+  await db.any(
+    `INSERT INTO ADMIN VALUES('${uuid()}', 'albus', '${MASTER_PASSWORD_HASH}', 'Albus', 'Dumbledore');`
+  )
+
+  // Insert 2 classes
+  const classes = ["level1", "level2"]
+
+  classes.forEach(async (className) => {
+    await db.any(`INSERT INTO CLASS VALUES ('${className}');`)
+  })
+
+  // Define 6 subjects (3 per class)
+  const subjects = {
+    level1: [
+      {
+        id: uuid(),
+        name: "Math",
+      },
+      {
+        id: uuid(),
+        name: "Physics",
+      },
+      {
+        id: uuid(),
+        name: "Chemistry",
+      },
+    ],
+    level2: [
+      {
+        id: uuid(),
+        name: "Math",
+      },
+      {
+        id: uuid(),
+        name: "Biology",
+      },
+      {
+        id: uuid(),
+        name: "History",
+      },
+    ],
+  }
+
+  // Insert all subjects for class "level1"
+  for (const subject of subjects.level1) {
+    await db.any(
+      `INSERT INTO SUBJECT VALUES ('${subject.id}', '${subject.name}');`
+    )
+    await db.any(`INSERT INTO OFFERS VALUES ('level1', '${subject.id}');`)
+  }
+
+  // Insert all subjects for class "level2"
+  for (const subject of subjects.level2) {
+    await db.any(
+      `INSERT INTO SUBJECT VALUES ('${subject.id}', '${subject.name}');`
+    )
+    await db.any(`INSERT INTO OFFERS VALUES ('level2', '${subject.id}');`)
+  }
+}
 
 async function initDB() {
   // Wait for some time to let the db init
@@ -31,10 +103,20 @@ async function initDB() {
     await db.any(allSqlQueries)
 
     console.log(chalk.green("Success! All tables created"))
-    process.exit()
   } catch (err) {
     console.error("Failed to execute SQL queries!", err)
+    return
   }
+
+  try {
+    await populateDB(db)
+    console.log(chalk.green("Success! Data inserted to db"))
+  } catch (err) {
+    console.error("Failed to populate database!", err)
+    return
+  }
+
+  process.exit()
 }
 
 initDB()
